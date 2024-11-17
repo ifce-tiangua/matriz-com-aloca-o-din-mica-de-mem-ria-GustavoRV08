45dfef5e82f1f10957ad66fc245ea74c36b32ae8
Gustavo RV <gustavorv102@gmail.com>
2024-11-17 19:03:32 -0300

    matriz dinamica

--- main.c
+++ main.c
@@ -5,17 +5,18 @@ int main(){
     int linhas, colunas;
     int** matriz;
     scanf("%d %d", &linhas, &colunas);
-    matriz = (int**) malloc (linhas*sizeof(int));
     if(linhas==0 || colunas==0){
         printf("[matriz vazia]");
         return 0;
     }
+    matriz = (int**) malloc (linhas*sizeof(int*));
     for(int i=0; i<linhas; i++){
-        matriz[i]=(int*) malloc(colunas*sizeof(int));
+        matriz[i]=(int*) malloc (colunas*sizeof(int));
     }
     for(int i=0; i<linhas; i++){
         for(int j=0; j<colunas; j++){
             scanf("%d", &matriz[i][j]);
+            fflush(stdin);
         }
     }
     for(int i=0; i<linhas; i++){
@@ -29,7 +30,7 @@ int main(){
         }
         printf("\n");
     }
-    for(int i=linhas; i>0; i--){
+    for(int i=0; i<linhas; i++){
         free(matriz[i]);
     }
     free(matriz);
